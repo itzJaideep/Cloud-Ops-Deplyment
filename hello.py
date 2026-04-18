4d81dce48c9060a1475d62f67ce11e20ebe19d75
Jaideep Malluvalasa <120393439+itzJaideep@users.noreply.github.com>
2026-04-18 14:31:18 +0530

    Convert script to Flask web application

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
-print("Hello, DevOps!")
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return f"Hello from Cloud-Ops Deploy! Host: {socket.gethostname()}"
+
+@app.route("/health")
+def health():
+    return {"status": "running"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
